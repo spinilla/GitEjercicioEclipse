ca80cd6fdeb85d5838c49ed3115ab8232c42646a
SERGIO PINILLA GARCIA <spinillag@viewnext.com>
2018-02-08 19:08:32 +0100

    Initial Commit

---
+++ src/com/viewnext/formacion/ClaseTest.java
@@ -0,0 +1,10 @@
+package com.viewnext.formacion;
+
+public class ClaseTest {
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+    System.out.println("Hola");
+  }
+
+}
